9d1903cbf0c7daf859a5d5d4f58db6e2898caa14
Emin Alemdar <77338109+eminalemdar@users.noreply.github.com>
2024-01-05 15:17:30 +0300

    fix typo

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
 output "arn" {
   description = "ARN of the bucket"
-  value       = module.example-s3-bucket.arn
+  value       = module.example-s3-module.arn
 }
 
 output "domain" {
   description = "Domain name of the bucket"
-  value       = module.example-s3-bucket.domain
+  value       = module.example-s3-module.domain
 }
