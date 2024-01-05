87b7bf19ca764e1f2cfebc72372d427720d2c907
Emin Alemdar <77338109+eminalemdar@users.noreply.github.com>
2024-01-05 15:07:28 +0300

    Initial commit

---
+++ main.tf
@@ -0,0 +1,8 @@
+module "example-s3-module" {
+  source  = "spacelift.io/emina/example-s3-module/default"
+  version = "0.0.1"
+
+  # Required inputs 
+  aws_region  = var.aws_region
+  bucket_name = var.bucket_name
+}
---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "aws_region" {
+  description = "AWS region"
+  type = string
+}
+
+variable "bucket_name" {
+  description = "The name of the S3 bucket. Must be unique"
+  type = string
+}
